0b7036bac69df09ba98f16e2ca1996dc5f723acf
Luke Bryan <luke@lukembryan.com>
2019-03-26 20:49:44 +0000

    project setup and testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TemplateHeaderComponent } from './template-header/template-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateHeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/template-header/template-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'template-header',
+  templateUrl: './template-header.component.html',
+  styleUrls: ['./template-header.component.less']
+})
+export class TemplateHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
