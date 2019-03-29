7e724e24a48ab14a3028c246f83ff6a37c534c75
Luke Bryan <luke@lukembryan.com>
2019-03-29 23:12:35 +0000

    more components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TemplateHeaderComponent } from './template-header/template-header.component';
+import { HeaderComponent } from './header/header.component';
 import { RecipeMasonryComponent } from './recipe-masonry/recipe-masonry.component';
+import { MenuComponent } from './menu/menu.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateHeaderComponent,
-    RecipeMasonryComponent
+    HeaderComponent,
+    RecipeMasonryComponent,
+    MenuComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
